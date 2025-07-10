10c01cf0bb28ba2df9480592a8ac7a1688d1a834
jiri-slanina <jirislanina96@gmail.com>
2025-07-10 10:03:41 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,14 @@ function infoIphone11() {
     "Přední kamera: 12MP<br>" +
     "Odolnost: IP68<br>" +
     "Face ID: Ano<br>" +
-    "Bezdrátové nabíjení: Ano"+
+    "Bezdrátové nabíjení: Ano<br>"+
     "Rozměry: 150.9 x 75.7 x 8.3mm<br>"+
     "Hmotnost: 194g";
+
+    document.getElementById("skrytBtn").style.display = "inline-block"
+}
+
+function smazInfo() {
+    document.getElementById("infoIphone11").textContent = ""
+    document.getElementById("skrytBtn").style.display = "none"
 }
