37717258289031989ebe25f34fa01dbc12198ff5
jiri-slanina <jirislanina96@gmail.com>
2025-07-10 12:11:30 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -8,10 +8,28 @@ function infoIphone11() {
     "Rozměry: 150.9 x 75.7 x 8.3mm<br>"+
     "Hmotnost: 194g";
 
-    document.getElementById("skrytBtn").style.display = "inline-block"
+    document.getElementById("skrytBtnIphone").style.display = "inline-block"
 }
 
 function smazInfo() {
     document.getElementById("infoIphone11").textContent = ""
-    document.getElementById("skrytBtn").style.display = "none"
+    document.getElementById("skrytBtnIphone").style.display = "none"
+}
+
+
+function iMacInfo() {
+    document.getElementById("infoIMac").innerHTML = "Procesor: Apple M3 chip (8jádrové CPU, 8jádrové nebo 10jádrové GPU)<br>"+
+    "Paměť (RAM): 8 GB / 16 GB / 24 GB jednotné paměti<br>"+
+    "Úložiště: SSD – 256 GB / 512 GB / 1 TB / 2 TB<br>"+
+    "Kamera: FaceTime HD (1080p)<br>"+
+    "Bezdrátové připojení: Wi-Fi 6E, Bluetooth 5.3<br>"+
+    "Rozměry: 547 × 461 × 147 mm (se stojanem)<br>"+
+    "Hmotnost: cca 4,5 kg<br>"
+
+    document.getElementById("skrytBtnIMac").style.display = "inline-block"
+}
+
+function skrytBtnIMac() {
+    document.getElementById("infoIMac"). innerHTML = ""
+    document.getElementById("skrytBtnIMac").style.display = "none"
 }
