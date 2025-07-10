4b48e5bc2cf8447f633b181614534cb9783a123a
jiri-slanina <jirislanina96@gmail.com>
2025-07-10 09:48:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function infoIphone11() {
+    document.getElementById("infoIphone11").innerHTML = "Procesor: A13 Bionic chip<br>"+
+    "Fotoaparát: zadní (12MP+12MP)<br>"+
+    "Přední kamera: 12MP<br>" +
+    "Odolnost: IP68<br>" +
+    "Face ID: Ano<br>" +
+    "Bezdrátové nabíjení: Ano"+
+    "Rozměry: 150.9 x 75.7 x 8.3mm<br>"+
+    "Hmotnost: 194g";
+}
